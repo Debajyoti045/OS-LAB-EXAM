314eae38dd2364c06fa309872808233c079b150c
Govind Singh <govindyadav2058@gmail.com>
2023-11-10 10:38:59 +0530

    FCFS Debugged

--- 1.FCFS.cpp
+++ 1.FCFS.cpp
@@ -1,72 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+// fcfs
+signed main()
 {
-    //No of processes
-    int n;
-    cin>>n;
-    // Arrival Time
-    vector<int>arrival(n);
-    // Burst Time
-    vector<int>length(n);
-    for(int i=0;i<n;i++){
-        cin>>arrival[i];
-        cin>>length[i];
-    }
-    vector<int>vis(n,0);
-    //Waiting,Turnaround
-    vector<int>waitingTime(n);
-    vector<int>TurnAroundTime(n);
-    queue<pair<int,int>>q;
-    // Push 0 arrival time process.
-    q.push({0,length[0]});
-    vis[0] = 1;
-    int time = 0;
-    while(!q.empty()){
-        auto it = q.front();
-        int process = it.first;
-        time+=it.second;
-        q.pop();
-      for(int i=0;i<n;i++){
-         if(time>=arrival[i] && !vis[i]){
-            q.push({i,length[i]});
-            vis[i] = 1;
-         }
-      }
-      TurnAroundTime[process] = time - arrival[process];
-      waitingTime[process] = TurnAroundTime[process] - length[process];
-    }
-    cout<<"Waiting time:"<<endl;
-    for(auto it:waitingTime) cout<<it<<" ";
-    cout<<endl;
-    cout<<"TurnAround time:"<<endl;
-    for(auto it:TurnAroundTime) cout<<it<<" ";
-    cout<<endl;
-    cout<<"Average Waiting Time: ";
-    float averageWait = 0.0;
-    for(auto it:waitingTime) averageWait+=it;
-    cout<<(float)(averageWait/n)<<endl;
-    
+	int n;
+	cin >> n;
+	vector<int> arrival(n), burstTime(n), turnaroundTime(n), waitingTime(n);
+	for(int i = 0; i < n; i++)
+		cin >> arrival[i] >> burstTime[i];
+	int currentTime = 0;
+	float avgWT = 0, avgTT = 0;
+	for(int i = 0; i < n; i++)
+	{
+		currentTime = max(currentTime, arrival[i]);
+		currentTime += burstTime[i];
+		turnaroundTime[i] = currentTime - arrival[i];
+        waitingTime[i] = turnaroundTime[i] - burstTime[i];
+        avgTT += turnaroundTime[i];
+        avgWT += waitingTime[i];
+	}
+	avgWT /= n;
+    avgTT /= n;
+    cout << "Waiting time:" << endl;
+    for(auto x : waitingTime) 
+        cout<< x <<" ";
+    cout << endl;
 
-    cout<<"Average TurnAround Time: ";
-    float averageTurn = 0.0;
-    for(auto it:TurnAroundTime) averageTurn+=it;
-    cout<<(float)(averageTurn/n)<<endl;
-// process = 5,arrival time=[0,0,2,8,12],length=[10,9,5,5,4]
-//Input
-// 5
-// 0 10
-// 0 9
-// 2 5
-// 8 5
-// 12 4
+    cout<<"TurnAround time:" << endl;
+    for(auto x:turnaroundTime) 
+        cout<< x <<" ";
+    cout << endl;
 
-//Output
-// Waiting time:
-// 0 10 17 16 17 
-// TurnAround time:
-// 10 19 22 21 21 
-// Average Waiting Time: 12
-// Average TurnAround Time: 18.6
-    return 0;
+    cout << "Average Waiting Time: " << avgWT << "\n";
+    cout << "Average TurnAround Time: " << avgTT << "\n";
+	return 0;
 }
